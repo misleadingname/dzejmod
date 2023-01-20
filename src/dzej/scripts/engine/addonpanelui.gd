0cc5f32624d050a9d8e98106c04ebc3d9dec4592
MichuButAnother <48837716+MichuButAnother@users.noreply.github.com>
2023-01-21 00:01:35 +0100

    backend stuff
    - Comment debug logging
    - Change a variable name
    - Fix typo
    - something else

--- src/dzej/scripts/engine/addonpanelui.gd
+++ src/dzej/scripts/engine/addonpanelui.gd
@@ -25,23 +25,23 @@ func setAddon(name):
 
 func explore_directory(dir: String):
 	var explorer: Directory = Directory.new()
-	dzej.msg(dir)
+	#dzej.msg(dir)
 
 	if explorer.open(dir) == OK:
 		if historicalpath != "":
 			historicalpath += "/" + dir
 		else:
 			historicalpath += dir
-		print(historicalpath)
+		#print(historicalpath)
 		explorer.list_dir_begin(true)
 
 		var next_victim = explorer.get_next()
 		while next_victim != "":
-			if explorer.current_is_dir() && next_victim != dir:
-				dzej.msg("Directory found: " + next_victim)
+			if explorer.current_is_dir():
+				#dzej.msg("Directory found: " + next_victim)
 				explore_directory(next_victim)
 			else:
-				dzej.msg(historicalpath + "/" + next_victim)
+				#dzej.msg(historicalpath + "/" + next_victim)
 				explorer.remove(historicalpath + "/" + next_victim)
 			next_victim = explorer.get_next()
 	else:
@@ -49,24 +49,20 @@ func explore_directory(dir: String):
 
 
 func _on_delete_button_down():
-	var ae: Directory = Directory.new()
-	if ae.open(realpath) == OK:
-		ae.list_dir_begin(true)
-		var victim = ae.get_next()
+	var dir: Directory = Directory.new()
+	if dir.open(realpath) == OK:
+		dir.list_dir_begin(true)
+		var victim = dir.get_next()
 		while victim != "":
-			if ae.current_is_dir():
-				dzej.msg("Found directory: " + victim)
+			if dir.current_is_dir():
+				#dzej.msg("Found directory: " + victim)
 				explore_directory(realpath + "/" + victim)
 				historicalpath = ""
-			else:
-				dzej.msg("Found file: " + victim)
-			ae.remove(realpath + "/" + victim)
-			victim = ae.get_next()
+				dir.remove(realpath + "/" + victim)
+			else: 
+				dir.remove(realpath + "/" + victim)
+			victim = dir.get_next()
 	else:
 		print("An error occurred when trying to access the path.")
-	ae.remove(realpath)
+	dir.remove(realpath)
 	dzej.lpMessageBox("Deleted an addon! \nRestart the game to apply changes.", "Alert!")
-
-	#dzej.msg(ae.remove(realpath))
-	#dzej.msg("[FATAL] restart required!")
-	#get_tree().quit()
